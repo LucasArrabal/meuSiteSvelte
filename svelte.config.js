09aeb18dd819c23ac9174f0f003ee2a95db4c491
p051520 <larrabal@prodam.sp.gov.br>
2024-03-19 13:09:32 -0300

    update 2

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.argv.includes('dev');
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
